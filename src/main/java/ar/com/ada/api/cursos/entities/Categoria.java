33b62ebec4c004724580d5d4facbd0b7a10390dc
Your Name <digneli14@gmail.com>
2020-07-18 14:19:03 +0200

    Se codeo los setters y getters a las demas clases y se creo el directorio repos y la inferfaz de Categoria repo! para hacer el testeo y pruebas de la creacion de las categorias.

--- src/main/java/ar/com/ada/api/cursos/entities/Categoria.java
+++ src/main/java/ar/com/ada/api/cursos/entities/Categoria.java
@@ -16,5 +16,29 @@ public class Categoria {
     @ManyToMany
     @JoinTable(name = "curso_x_categoria", joinColumns = @JoinColumn(name = "categoria_id"), inverseJoinColumns = @JoinColumn(name = "curso_id"))
     private List<Curso> cursos;
+	public Integer getCategoriaId() {
+		return categoriaId;
+	}
+	public void setCategoriaId(Integer categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public List<Curso> getCursos() {
+		return cursos;
+	}
+	public void setCursos(List<Curso> cursos) {
+		this.cursos = cursos;
+	}
 
 }
